e90f14f84fd57bb2794acdb5e1e6cacc68e0b381
John Bocalig <john@sagetree.net>
2020-04-06 10:02:55 -0700

    OCMC01-62: Reworked the mission priests geofield map input field to be like missionaries and mission teams. Styled the mission priests map to match the styles on the homepage.

--- web/modules/custom/ocmc_mission/js/ocmc_mission_disable_number_input_scrolling.js
+++ web/modules/custom/ocmc_mission/js/ocmc_mission_disable_number_input_scrolling.js
@@ -1,7 +1,6 @@
 (function ($, Drupal) {
   Drupal.behaviors.ocmc_mission_disable_number_input_scrolling = {
     attach: function (context, settings) {
-      console.log('kurt loaded');
       $(document).on("wheel", "input[type=number]", function (e) {
         $(this).blur();
       });
